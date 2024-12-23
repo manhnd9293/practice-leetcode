57318ba248df4deb706ffb0aedb11e9552e4fcf6
Manh Nguyen <manhnd9293@gmail.com>
2024-12-23 23:55:37 +0700

    547. Number of Provinces

---
+++ union-find/findCircleNum.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+var findCircleNum = function(isConnected) {
+  const n = isConnected.length;
+  const root = Array.from({length: n}, (_, index) => index);
+  const rank = Array(n).fill(1);
+
+  function union(x, y) {
+    const rootX = root[x];
+    const rootY = root[y];
+
+    if (rank[rootX] > rank[rootY]) {
+      root[rootY] = rootX;
+    } else if (rank[rootX] < rank[rootY]) {
+      root[rootX] = rootY;
+    } else {
+      root[rootY] = rootX;
+      rank[rootX] += 1;
+    }
+  }
+
+  function find(x) {
+    if (x === root[x]) {
+      return x;
+    }
+    root[x] = find(root[x]);
+    return root[x];
+  }
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (i !== j && isConnected[i][j] === 1) {
+        union(i, j);
+      }
+    }
+  }
+
+  let ans = 0;
+  for (let i = 0; i < n; i++) {
+    if (root[i] === i) {
+      ans++;
+    }
+  }
+
+  return ans;
+};
